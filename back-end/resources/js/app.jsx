23579980dfb67a4b52376540a4dd63b8c9d8a4d3
Sajed Zaitoun <abcsajed@gmail.com>
2026-02-10 13:03:29 +0100

    making one server for laravel and react

---
+++ back-end/resources/js/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+    return <h1>React + Laravel 🚀</h1>;
+}
+
+createRoot(document.getElementById("app")).render(<App />);
